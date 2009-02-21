fee8274e8694ac55f75b3efb5d845be309d6948f
kkrizka <kkrizka@64adfc07-a82a-0410-b257-a30718e9be3b>
2009-02-21 22:26:51 +0000

    Plugin based AMELIA, initial import: - AStart plugin 	- Plays a quick preview movie 	- Can be skilled with Escape/Space/Enter - ABase plugin 	- Used for switching between monitors 	- Different monitor groups 	- Shows live snapshots of the widget with QWidgetProxy 	- Animated transitions - AGeometry plugin 	- The ageometry layout - AHistogram plugin 	- ROOT based histogramming plugin 	- Can display the distribution of Pt in an event 	- Can create histograms that track combinations are added to 	- Still needs a lot of work - AEventManager plugin 	- Contains xmlevents 	- Does not depends on any display stuff 	- Possible to have a complete list of tracks in an event 	- Possible to filter events based on programmed tasks, updates live 	- Contains event manager widgets - AWeb plugin 	- Monitors that display one single webpage

--- config.h.cmake
+++
@@ -1,4 +0,0 @@
-#define SHARE_PREFIX "${CMAKE_INSTALL_PREFIX}/share/amelia"
-#define MEDIA_PREFIX "${CMAKE_INSTALL_PREFIX}/share/amelia/media"
-#define TOURS_PREFIX "${CMAKE_INSTALL_PREFIX}/share/amelia/tours"
-#define EVENTS_PREFIX "${CMAKE_INSTALL_PREFIX}/share/amelia/events"

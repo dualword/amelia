e074f18a6701c7b29014c59d4567b493af10eef2
kkrizka <kkrizka@64adfc07-a82a-0410-b257-a30718e9be3b>
2009-05-31 09:36:19 +0000

    libamelia - Added a new class AAnimationLayoutGUI which handles animating widgets that are part of the layout. It inherits from AAnimationGUI and the only change it adds is that the final position of the animation will always be the one that is in the layout.
    ageometry
    - Removed minimum size requirement for the main window.
    - Use AAnimationLayoutGUI for animating the toolbar thing. This results in better looking final layout and is responsive to resizing of the UI.
    - Show the event toolbar thing after all 3D tracks have been created. This results in a smoother animation.

---
+++ trunk/src/libamelia/AAnimationLayoutGUI.cpp
@@ -0,0 +1,86 @@
+#include "AAnimationLayoutGUI.h"
+
+#include <QDebug>
+#include <QApplication>
+
+AAnimationLayoutGUI::AAnimationLayoutGUI(QObject* parent)
+  : AAnimationGUI(parent),layout(0)
+{ }
+
+AAnimationLayoutGUI::AAnimationLayoutGUI(QWidget* w,QObject* parent)
+  : AAnimationGUI(parent),layout(0)
+{ 
+  setWidget(w);
+}
+
+AAnimationLayoutGUI::~AAnimationLayoutGUI()
+{
+}
+
+void AAnimationLayoutGUI::setWidget(QWidget* w)
+{
+  AAnimationGUI::setWidget(w);
+
+  //Calculate the final position of the widget based on its layed out
+  QWidget *parent=w->parentWidget();
+  layout=parent->layout();
+  layout=findLayout(layout);
+  AAnimationGUI::setPosAt(1,w->pos());
+}
+
+void AAnimationLayoutGUI::setTimeLine(QTimeLine *timeLine)
+{
+  if(this->timeLine()!=0)
+    disconnect(this->timeLine(),SIGNAL(finished()),
+	       this,SLOT(handleFinished()));
+
+  AAnimationGUI::setTimeLine(timeLine);
+
+  if(this->timeLine()!=0)
+    connect(this->timeLine(),SIGNAL(finished()),
+	    this,SLOT(handleFinished()));
+}
+
+void AAnimationLayoutGUI::setPosAt(qreal time,QPoint p)
+{
+  if(time==1) return; //Final position is dictated by the layout
+  AAnimationGUI::setPosAt(time,p);
+}
+
+
+QLayout* AAnimationLayoutGUI::findLayout(QLayout *layout)
+{
+  if(layout->indexOf(widget())!=-1) return layout;
+  
+  for(int i=0;i<layout->count();i++)
+    {
+      QLayoutItem *item=layout->itemAt(i);
+      QLayout *sublayout=item->layout();
+      if(sublayout)
+	{
+	  QLayout *found=findLayout(sublayout);
+	  if(found) return found;
+	}
+    }
+}
+
+void AAnimationLayoutGUI::handleFinished()
+{
+  //layout->setEnabled(true);
+  layout->addWidget(widget());
+}
+
+void AAnimationLayoutGUI::setStep(qreal step)
+{
+  //if(!widget()->isVisible()) //We need to disable the layout on the first go 
+    //layout->setEnabled(false);
+  
+  AAnimationGUI::setStep(step);
+  if(!widget()->isVisible())
+    {
+      //layout->setEnabled(false);
+      layout->removeWidget(widget());
+      widget()->show();
+    }
+
+}
